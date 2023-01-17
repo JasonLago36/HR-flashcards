2b7e685695ea2fb1d17c41c126122dbb870e3f96
Christian Wu <christianwu56@gmail.com>
2023-01-17 12:56:10 -0800

    refactored backend code

--- backend/flashcard/views.py
+++ backend/flashcard/views.py
@@ -1,12 +1,22 @@
-from django.http import HttpResponse
-from django.template import loader
+# from django.http import HttpResponse
+# from django.template import loader
 from flashcard.models import Flashcard
-# Create your views here.
+from django.shortcuts import render
+
+
+# This is a bit easier code for rendering out your view functions
 
 def flashcard (request):
-    flashcards = Flashcard.objects.all().values()
-    template = loader.get_template('flashcards.html')
+    flashcards = Flashcard.objects.all()
     context = {
-        'flashcards':flashcards
+        'flashcards': flashcards
     }
-    return HttpResponse(template.render(context,request))
+    return render(request, 'flashcard/flasher.html', context)
+
+
+    # flashcards = Flashcard.objects.all().values()
+    # template = loader.get_template('flashcards.html')
+    # context = {
+    #     'flashcards':flashcards
+    # }
+    # return HttpResponse(template.render(context,request))
